cf5ec67dc3434f8d31aa2cc8913da1c3d21a2582
Heidi R <hgr7@nau.edu>
2019-05-29 15:41:44 -0700

    make first draft of site map on dtm with alaska inset

--- Site_Overview/Site_Overview_Map.R
+++ Site_Overview/Site_Overview_Map.R
@@ -11,6 +11,7 @@ library(raster)
 library(ggmap)
 library(ggthemes)
 library(viridis)
+library(RStoolbox)
 ##############################################################################################################
 
 ### Load Data ################################################################################################
@@ -25,7 +26,15 @@ emldtm <- raster::merge(raster(filenames[1]),
                         raster(filenames[2]),
                         raster(filenames[3]),
                         raster(filenames[4])) # only the four tiles right around eml
-filenames <- list.files('C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/Remote Sensing/NEON/Airborne_Data_2017/DTMGtif', full.names = TRUE)
+filenames <- c('C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/Remote Sensing/NEON/Airborne_Data_2018/2018_HEAL_RGB/L3/Camera/Mosaic/V01/2018_HEAL_2_389000_7085000_image.tif',
+               'C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/Remote Sensing/NEON/Airborne_Data_2018/2018_HEAL_RGB/L3/Camera/Mosaic/V01/2018_HEAL_2_389000_7086000_image.tif',
+               'C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/Remote Sensing/NEON/Airborne_Data_2018/2018_HEAL_RGB/L3/Camera/Mosaic/V01/2018_HEAL_2_390000_7085000_image.tif',
+               'C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/Remote Sensing/NEON/Airborne_Data_2018/2018_HEAL_RGB/L3/Camera/Mosaic/V01/2018_HEAL_2_390000_7086000_image.tif')
+emlrgb <- raster::merge(brick(filenames[1]),
+                        brick(filenames[2]),
+                        brick(filenames[3]),
+                        brick(filenames[4])) # only the four tiles right around eml
+# filenames <- list.files('C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/Remote Sensing/NEON/Airborne_Data_2017/DTMGtif', full.names = TRUE)
 # neondtm <- raster(filenames[1])
 # for (i in 2:length(filenames)) {
 #   neondtm <- raster::merge(neondtm, raster(filenames[i]))
@@ -34,14 +43,27 @@ filenames <- list.files('C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur L
 
 ### Select/clip to CiPEHR ####################################################################################
 transects2009 <- points2009 %>%
-  filter(type == 'fence' | type == 'trans')
+  filter(type == 'fence' | type == 'trans') %>%
+  st_transform(crs = st_crs(emldtm))
 
 transects2018 <- points2018 %>%
-  mutate(Name = as.numeric(Name)) %>%
-  filter(Name > 10000 & Name < 13000)
+  st_zm() %>%
+  mutate(Name = as.numeric(as.character(Name))) %>%
+  filter(Name > 10000 & Name < 13000) %>%
+  st_transform(crs = st_crs(emldtm))
 
 cipehrblocks <- emlpoints %>%
   filter(Exp == 'CiPEHR') %>%
+  st_transform(crs = st_crs(emldtm))
+
+cipehr <- cipehrblocks %>%
+  st_transform(crs = 4326) %>%
+  cbind.data.frame(st_coordinates(.)) %>%
+  group_by(Exp) %>%
+  summarise(X = mean(X),
+            Y = mean(Y))
+
+cipehrblocks <- cipehrblocks %>%
   cbind.data.frame(st_coordinates(.))
 
 plot(emldtm)
@@ -49,23 +71,58 @@ emlslope <- terrain(emldtm, opt = 'slope')
 plot(emlslope)
 emlaspect <- terrain(emldtm, opt = 'aspect')
 plot(emlaspect)
-emlhillshd <- hillShade(emlslope, emlaspect)
+emlhillshd <- hillShade(emlslope, emlaspect, direction = 45)
 plot(emlhillshd)
+rm(emlslope, emlaspect)
 ##############################################################################################################
 
 ### Convert raster to dataframe for plotting with ggplot #####################################################
 emlhillshd.df <- as.data.frame(emlhillshd, xy = TRUE)
+emlrgb <- reclassify(emlrgb, cbind(NA, -9999))
+##############################################################################################################
 
 ### Plot block location over hillshade #######################################################################
-# need to transform to have the same cs before this will work!
-ggplot(emlhillshd.df, aes(x = x, y = y, fill = layer)) +
+# emlimage <- ggRGB(emlrgb, r = 1, g = 2, b = 3, stretch = 'lin')
+# 
+# emlimage +
+#   geom_point(data = cipehrblocks, aes(x = X, y = Y), inherit.aes = FALSE) +
+#   theme_few() +
+#   scale_fill_gradient(low = '#FFFFFF', high = '#000000', guide = FALSE) +
+#   theme(axis.title = element_blank(),
+#         axis.text.x  = element_text(size = 8),
+#         axis.text.y = element_text(size = 8),
+#         aspect.ratio = 1)
+
+akcenter <- as.numeric(geocode('alaska, usa'))
+akmap <- ggmap(get_googlemap(center = akcenter, zoom = 4, maptype = 'satellite'))
+
+akmapgrob <- ggplotGrob(akmap +
+  geom_point(data = cipehr, aes(x = X, y = Y), inherit.aes = FALSE, colour = '#cc3300') +
+  theme_map() +
+  theme(axis.title = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        plot.margin = grid::unit(c(0,0,0,0),"null"),
+        panel.background = element_rect(fill = NULL)))
+
+emlmap <- ggplot(emlhillshd.df, aes(x = x, y = y, fill = layer)) +
   geom_tile() +
-  geom_point(data = cipehrblocks, aes(x = X, y = Y), inherit.aes = FALSE) +
+  geom_point(data = cipehrblocks, aes(x = X, y = Y), inherit.aes = FALSE, colour = '#cc3300') +
+  geom_text(data = cipehrblocks, aes(x = X, y = Y, label = Block), inherit.aes = FALSE, colour = '#cc3300',  hjust = -0.2, vjust = -0.2) +
   theme_few() +
-  scale_fill_gradient(low = '#FFFFFF', high = '#000000') +
+  scale_fill_gradient(low = '#000000', high = '#DDDDDD',
+                      guide = FALSE) +
+  scale_x_continuous(limits = c(389000, 391000),
+                     expand = c(0,0)) +
+  scale_y_continuous(limits = c(7085000, 7087000),
+                     expand = c(0,0)) +
   theme(axis.title = element_blank(),
         axis.text.x  = element_text(size = 8),
         axis.text.y = element_text(size = 8),
-        legend.text = element_blank(),
         aspect.ratio = 1) +
   coord_fixed()
+emlmap
+
+fullmap <- emlmap +
+  annotation_custom(grob = akmapgrob, xmin = 389000, ymin = 7085000, xmax = 389500, ymax = 7085500)
+fullmap
