0b666276b66ab3a47cbf0543074129d800bec813
Heidi R <hgr7@nau.edu>
2019-05-28 17:23:33 -0700

    more work on site overview map but it's not working yet

--- Site_Overview/Site_Overview_Map.R
+++ Site_Overview/Site_Overview_Map.R
@@ -8,18 +8,28 @@ library(sf)
 library(tidyverse)
 library(sp)
 library(raster)
+library(ggmap)
+library(ggthemes)
+library(viridis)
 ##############################################################################################################
 
 ### Load Data ################################################################################################
 points2009 <- st_read('C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/GPS/All_Points/All_Points_2009_SPCSAK4_corrected.shp')
 points2018 <- st_read('C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/GPS/All_Points/All_Points_2018_SPCSAK4.shp')
 emlpoints <- st_read('C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/GPS/All_Points/Site_Summary_Shapefiles/Sites.shp')
-emldtm <- raster('C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/Remote Sensing/NEON/Airborne_Data_2017/DTM_All/eml_dtm')
+filenames <- c('C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/Remote Sensing/NEON/Airborne_Data_2017/DTMGtif/NEON_D19_HEAL_DP3_389000_7085000_DTM.tif',
+               'C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/Remote Sensing/NEON/Airborne_Data_2017/DTMGtif/NEON_D19_HEAL_DP3_389000_7086000_DTM.tif',
+               'C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/Remote Sensing/NEON/Airborne_Data_2017/DTMGtif/NEON_D19_HEAL_DP3_390000_7085000_DTM.tif',
+               'C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/Remote Sensing/NEON/Airborne_Data_2017/DTMGtif/NEON_D19_HEAL_DP3_390000_7086000_DTM.tif')
+emldtm <- raster::merge(raster(filenames[1]),
+                        raster(filenames[2]),
+                        raster(filenames[3]),
+                        raster(filenames[4])) # only the four tiles right around eml
 filenames <- list.files('C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/Remote Sensing/NEON/Airborne_Data_2017/DTMGtif', full.names = TRUE)
-neondtm <- raster(filenames[1])
-for (i in 2:length(filenames)) {
-  neondtm <- raster::merge(neondtm, filenames[i])
-}
+# neondtm <- raster(filenames[1])
+# for (i in 2:length(filenames)) {
+#   neondtm <- raster::merge(neondtm, raster(filenames[i]))
+# }
 ##############################################################################################################
 
 ### Select/clip to CiPEHR ####################################################################################
@@ -31,7 +41,8 @@ transects2018 <- points2018 %>%
   filter(Name > 10000 & Name < 13000)
 
 cipehrblocks <- emlpoints %>%
-  filter(Exp == 'CiPEHR')
+  filter(Exp == 'CiPEHR') %>%
+  cbind.data.frame(st_coordinates(.))
 
 plot(emldtm)
 emlslope <- terrain(emldtm, opt = 'slope')
@@ -39,21 +50,22 @@ plot(emlslope)
 emlaspect <- terrain(emldtm, opt = 'aspect')
 plot(emlaspect)
 emlhillshd <- hillShade(emlslope, emlaspect)
+plot(emlhillshd)
 ##############################################################################################################
-plot(emldtm)
-r <- raster(ncol=3, nrow=3)
-r[] <- sqrt(1:ncell(r))
-plot(r)
-r[3:5] <- NA
-test <- as.data.frame(r)
-s <- stack(r, r*2)
-as.data.frame(s)
-as.data.frame(s, na.rm=TRUE)
 
 ### Convert raster to dataframe for plotting with ggplot #####################################################
 emlhillshd.df <- as.data.frame(emlhillshd, xy = TRUE)
-  as(emlshd, "SpatialPixelsDataFrame") %>% # convert to spatial pixels dataframe
-  as.data.frame() %>% # convert to dataframe
-  rename(value = 1)
 
 ### Plot block location over hillshade #######################################################################
+# need to transform to have the same cs before this will work!
+ggplot(emlhillshd.df, aes(x = x, y = y, fill = layer)) +
+  geom_tile() +
+  geom_point(data = cipehrblocks, aes(x = X, y = Y), inherit.aes = FALSE) +
+  theme_few() +
+  scale_fill_gradient(low = '#FFFFFF', high = '#000000') +
+  theme(axis.title = element_blank(),
+        axis.text.x  = element_text(size = 8),
+        axis.text.y = element_text(size = 8),
+        legend.text = element_blank(),
+        aspect.ratio = 1) +
+  coord_fixed()
