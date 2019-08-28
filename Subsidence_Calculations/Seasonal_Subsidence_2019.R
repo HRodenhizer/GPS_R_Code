ca9efbfb0419c3b4726e15b5275dfdab1561bf27
Heidi R <hgr7@nau.edu>
2019-08-28 16:44:27 -0700

    process Aug 2019 gps data for interannual subsidence and start processing Aug 2019 gps data for seasonal subsidence

--- Subsidence_Calculations/Seasonal_Subsidence_2019.R
+++ Subsidence_Calculations/Seasonal_Subsidence_2019.R
@@ -14,21 +14,34 @@ library(RStoolbox)
 ##############################################################################################################
 
 ### Load data ################################################################################################
-filenames <- list.files('C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/GPS/Kriged_Surfaces/Elevation_Variance/Uncorrected_kriged_elevation',
-                        pattern = '[ABC]201[89]K.tif',
+filenames <- list.files('C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/GPS/Kriged_Surfaces/Elevation_Variance/ALT_Sub_Ratio_Corrected',
+                        pattern = '[ABC]201[89].*\\.tif',
                         full.names = TRUE)
-elevation <- list(brick(stack(filenames[1:2])),
-                  brick(stack(filenames[3:4])),
-                  brick(stack(filenames[5:6])))
+elevation <- list(brick(stack(filenames[which(str_detect(filenames, 'A201'))])),
+                  brick(stack(filenames[which(str_detect(filenames, 'B201'))])),
+                  brick(stack(filenames[which(str_detect(filenames, 'C201'))])))
+names(elevation) <- c('Block A', 'Block B', 'Block C')
+names(elevation[[1]]) <- c('Aug2018', 'May2019', 'Aug2019')
+names(elevation[[2]]) <- c('Aug2018', 'May2019', 'Aug2019')
+names(elevation[[3]]) <- c('Aug2018', 'May2019', 'Aug2019')
 Points2018 <- st_read("C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/GPS/All_Points/All_Points_2018_SPCSAK4.shp")
-Points2019 <- st_read("C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/GPS/All_Points/All_Points_May_2019_SPCSAK4.shp")
+Points052019 <- st_read("C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/GPS/All_Points/All_Points_May_2019_SPCSAK4.shp")
+Points082019 <- st_read("C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/GPS/All_Points/All_Points_08_2019_SPCSAK4.shp")
 grids <- list(clearValues(raster('C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/GPS/Kriged_Surfaces/Elevation_Variance/Uncorrected_kriged_elevation/A2017K.tif')),
               clearValues(raster('C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/GPS/Kriged_Surfaces/Elevation_Variance/Uncorrected_kriged_elevation/B2017K.tif')),
               clearValues(raster('C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/GPS/Kriged_Surfaces/Elevation_Variance/Uncorrected_kriged_elevation/C2017K.tif')))
 ##############################################################################################################
 
 ### calculate elevation change ###############################################################################
-frost_heave <- map(elevation, ~.x[[2]] - .x[[1]])
+elevation_start <- map(elevation, ~subset(.x, 1:2))
+elevation_end <- map(elevation, ~subset(.x, 2:3))
+elev_change <- map2(elevation_end, elevation_start, ~.x-.y)
+walk(elev_change, ~plot(.x))
+elev_change_df <- map2_dfr(elev_change,
+                           c('a', 'b', 'c'),
+                           ~as.data.frame(.x, xy = TRUE) %>%
+                             gather(key = collection, value = dElev, May2019:Aug2019) %>%
+                             mutate(block = .y))
 
 # filenames <- paste('C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/GPS/Kriged_Surfaces/Subsidence_Clipped/Seasonal_Sub/',
 #                    titles,
@@ -45,49 +58,84 @@ points18xy <- Points2018 %>%
   filter(Name > 10000 & Name < 13000) %>%
   arrange(Name)
 
-points19xy <- Points2019 %>%
+points0519xy <- Points052019 %>%
+  st_zm() %>%
+  mutate(Name = as.numeric(as.character(Name))) %>%
+  filter(Name > 10000 & Name < 13000) %>%
+  arrange(Name)
+
+points0819xy <- Points082019 %>%
   st_zm() %>%
   mutate(Name = as.numeric(as.character(Name))) %>%
   filter(Name > 10000 & Name < 13000) %>%
   arrange(Name)
 
 # calculate the distance between paired gps points
-point_distances <- points19xy %>%
+point_distances <- points0819xy %>%
   select(Name, Easting, Northing) %>%
-  cbind.data.frame(data.frame(distance = as.numeric(st_distance(points18xy, points19xy, by_element = TRUE)))) %>%
+  cbind.data.frame(data.frame(distance0519 = as.numeric(st_distance(points18xy, points0519xy, by_element = TRUE)))) %>%
+  cbind.data.frame(data.frame(distance0819 = as.numeric(st_distance(points0519xy, points0819xy, by_element = TRUE)))) %>%
   st_as_sf() %>%
   mutate(block = ifelse(Name > 10000 & Name < 11000,
                         'a',
                         ifelse(Name > 11000 & Name < 12000,
                                'b',
-                               'c')))
+                               'c'))) %>%
+  gather(key = collection, value = distance, distance0519:distance0819) %>%
+  mutate(collection = ifelse(collection == 'distance0519',
+                             'May2019',
+                             'Aug2019'))
 
-mean_distance <- mean(point_distances$distance, na.rm = TRUE)
-sd_distance <- sd(point_distances$distance, na.rm = TRUE)
-max_distance <- max(point_distances$distance, na.rm = TRUE)
-min_distance <- min(point_distances$distance, na.rm = TRUE)
+distance_summary_stats <- point_distances %>%
+  st_drop_geometry() %>%
+  group_by(collection) %>% 
+  summarise(mean = mean(distance, na.rm = TRUE),
+            sd = sd(distance, na.rm = TRUE),
+            max = max(distance, na.rm = TRUE),
+            min = min(distance, na.rm = TRUE))
 
-ggplot(point_distances, aes(x = block, y = distance)) +
+ggplot(point_distances, aes(x = block, y = distance, fill = collection)) +
   geom_boxplot()
 
 # turn the distances into a list for using the map function
-distance_list <- list(filter(point_distances, block == 'a'),
-                      filter(point_distances, block == 'b'),
-                      filter(point_distances, block == 'c'))
+point_distances <- st_drop_geometry(point_distances)
+distance_list <- list(list(filter(point_distances, block == 'a' & collection == 'May2019'),
+                           filter(point_distances, block == 'a' & collection == 'Aug2019')),
+                      list(filter(point_distances, block == 'b' & collection == 'May2019'),
+                           filter(point_distances, block == 'b' & collection == 'Aug2019')),
+                      list(filter(point_distances, block == 'c' & collection == 'May2019'),
+                           filter(point_distances, block == 'c' & collection == 'Aug2019')))
 
-# turn the distances into a raster and join with the frost heave raster
-distance_raster <- map2(distance_list, grids, ~ rasterize(st_drop_geometry(.x)[,2:3], .y, field = st_drop_geometry(.x)[,4]))
-frost_heave <- map2(frost_heave, distance_raster, ~ brick(stack(.x, .y)))
+# turn the distances into a raster
+distance_raster <- list()
+for (i in 1:length(distance_list)) {
+  distance_raster[[i]] <- raster()
+  for (k in 1:length(distance_list[[i]])) {
+    print(paste(i, k, sep = '-'))
+    temp <- rasterize(distance_list[[i]][[k]][,2:3], grids[[i]], field = distance_list[[i]][[k]][,6])
+    print('after raster')
+    distance_raster[[i]] <- stack(distance_raster[[i]], temp)
+  }
+  distance_raster[[i]] <- brick(distance_raster[[i]])
+  names(distance_raster[[i]]) <- c('May2019', 'Aug2019')
+  plot(distance_raster[[i]])
+  rm(i, k)
+}
 
-# turn the frost heave/distance raster into a data frame for mapping
-frost_heave_df <- map(frost_heave, ~ fortify(.x) %>% 
-                        rename(d.elev = layer.1, point.dist = layer.2) %>%
-                        mutate(d.elev.2 = ifelse(point.dist < 0.2,
-                                                 d.elev,
-                                                 NA),
-                               d.elev.1 = ifelse(point.dist < 0.1,
-                                                 d.elev,
-                                                 NA)))
+
+# turn the distance raster into a data frame for joining with the elevation change raster and mapping
+distance_df <- map2_dfr(distance_raster,
+                        c('a', 'b', 'c'),
+                        ~ fortify(.x) %>% 
+                          gather(key = collection, value = point.dist, May2019:Aug2019) %>%
+                          mutate(block = .y))
+##############################################################################################################
+
+### Join elevation change and point distance dataframes ######################################################
+elev_change_df <- elev_change_df %>%
+  full_join(distance_df, by = c('x', 'y', 'block', 'collection')) %>%
+  select(x, y, block, collection, dElev, point.dist)
+##############################################################################################################
 
 # find the mean frost heave for each block for all cells, for all cells with gps points within 20 cm, 
 # and for all cells with gps points within 10 cm
--- Subsidence_Calculations/Subsidence_Maps_Gap_Filled.R
+++ Subsidence_Calculations/Subsidence_Maps_Gap_Filled.R
@@ -17,9 +17,16 @@ library(viridis)
 
 ### Load data ##############################################################################################
 # find elevation files in directory
-filenames <- list.files("C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/GPS/Kriged_Surfaces/Elevation_Variance/ALT_Sub_Ratio_Corrected", full.names = TRUE, pattern = '^.*.tif$')
+filenames <- list.files("C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/GPS/Kriged_Surfaces/Elevation_Variance/ALT_Sub_Ratio_Corrected", full.names = TRUE, pattern = '[[:alpha:]]{1}[[:digit:]]{4}[[:alpha:]]+\\.tif$')
 # make a list of elevation raster stacks for each block
-Elevation <- list(stack(filenames[1:6]), stack(filenames[7:12]), stack(filenames[13:18]))
+Elevation <- list
+blocks <- readOGR('C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/GPS/All_Points/Site_Summary_Shapefiles/Blocks_Poly.shp')
+blocks11 <- readOGR('C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/GPS/All_Points/Site_Summary_Shapefiles/Blocks_Poly_2011.shp')
+############################################################################################################
+
+### Write raster stacks of unfilled unclipped data #########################################################
+# names <- paste("C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/GPS/Kriged_Surfaces/Elevation_Variance/ALT_Sub_Ratio_Corrected/Elevation_Stacks/", c('A', 'B', 'C'), 'ElevStack_unfilled_unclipped.tif', sep = '')
+# walk2(Elevation, names, ~writeRaster(.x, .y))
 ############################################################################################################
 
 ### Create empty rasters for missing years and fill values using a spline ##################################
@@ -41,22 +48,42 @@ for (i in 1:length(Elevation)){
                                 Elevation[[i]][[3]], #2015
                                 Elevation[[i]][[4]], #2016
                                 Elevation[[i]][[5]], #2017
-                                Elevation[[i]][[6]]) #2018
-  Elevation_fill[[i]] <- mask(approxNA(brick_missing_values), blocks, xy = TRUE)
+                                Elevation[[i]][[6]], #2018
+                                Elevation[[i]][[7]]) #2019
+  Elevation_fill[[i]] <- approxNA(brick_missing_values)
   plot(Elevation_fill[[i]])
   rm(i, empty_raster, brick_missing_values) # remove the index value and intermediate steps
 }
+# because these are unclipped there will be some cells around the margins with bad filled data where a value
+# was fit witout data on both sides (temporally)
 ############################################################################################################
 
-### Write raster stacks with all years #####################################################################
-# writeRaster(Elevation_fill[[1]], 'C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/GPS/Kriged_Surfaces/Elevation_Variance/ALT_Sub_Ratio_Corrected/Elevation_Stacks/AElevationStack_unclipped.tif')
-# writeRaster(Elevation_fill[[2]], 'C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/GPS/Kriged_Surfaces/Elevation_Variance/ALT_Sub_Ratio_Corrected/Elevation_Stacks/BElevationStack_unclipped.tif')
-# writeRaster(Elevation_fill[[3]], 'C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/GPS/Kriged_Surfaces/Elevation_Variance/ALT_Sub_Ratio_Corrected/Elevation_Stacks/CElevationStack_unclipped.tif')
+### Write filled unclipped rasters #########################################################################
+# names <- paste("C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/GPS/Kriged_Surfaces/Elevation_Variance/ALT_Sub_Ratio_Corrected/Elevation_Stacks/", c('A', 'B', 'C'), 'ElevStack_filled_unclipped.tif', sep = '')
+# walk2(Elevation_fill, names, ~writeRaster(.x, .y))
 ############################################################################################################
 
-### Graph Latest Subsidence ################################################################################
-filenames <- list.files('C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/GPS/Kriged_Surfaces/Elevation_Variance/ALT_Sub_Ratio_Corrected/Elevation_Stacks', full.names = TRUE)
-Elevation_fill <- list(brick(filenames[1]), brick(filenames[3]), brick(filenames[5]))
+### clip filled rasters to areas with data for all years ###################################################
+Elevation_fill_clipped <- map(Elevation_fill, ~mask(.x, blocks, xy = TRUE)) # why does this not mask when I use walk instead of map?
+############################################################################################################
+
+### Write filled clipped rasters #########################################################################
+# names <- paste("C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/GPS/Kriged_Surfaces/Elevation_Variance/ALT_Sub_Ratio_Corrected/Elevation_Stacks/", c('A', 'B', 'C'), 'ElevStack_filled_clipped.tif', sep = '')
+# walk2(Elevation_fill_clipped, names, ~writeRaster(.x, .y))
+############################################################################################################
+
+### Calculate Latest Subsidence (CiPEHR) ###################################################################
+filenames <- list.files('C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/GPS/Kriged_Surfaces/Elevation_Variance/ALT_Sub_Ratio_Corrected/Elevation_Stacks', 
+                        full.names = TRUE,
+                        pattern = '[ABC]ElevStack_filled_clipped.tif')
+Elevation_fill <- list(brick(filenames[which(str_detect(filenames, 'AElevStack'))]), 
+                       brick(filenames[which(str_detect(filenames, 'BElevStack'))]), 
+                       brick(filenames[which(str_detect(filenames, 'CElevStack'))]))
+names(Elevation_fill) <- list('Block A', 'Block B', 'Block C')
+names(Elevation_fill[[1]]) <- paste('A', seq(2009, 2019), sep = '_')
+names(Elevation_fill[[2]]) <- paste('B', seq(2009, 2019), sep = '_')
+names(Elevation_fill[[3]]) <- paste('C', seq(2009, 2019), sep = '_')
+
 Fences <- readOGR('C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/GPS/All_Points/Site_Summary_Shapefiles/Fences.shp')
 Fences <- fortify(Fences) %>%
   mutate(fence = as.numeric(id) + 1,
@@ -66,26 +93,25 @@ Fences <- fortify(Fences) %>%
                                'C',
                                'B')))
 
-
-sub_2018 <- list()
+sub <- list()
 Fences_norm <- data.frame()
-sub_2018_df <- data.frame()
+sub_df <- data.frame()
 avg_sub <- data.frame()
 for (i in 1:length(Elevation_fill)) {
   block.names <- c('A', 'B', 'C')
-  sub_2018[[i]] <- as(Elevation_fill[[i]][[10]] - Elevation_fill[[i]][[1]], "SpatialPixelsDataFrame")
-  temp_sub <- sub_2018[[i]] %>%
-    as.data.frame() %>%
-    rename(subsidence = layer) %>%
-    mutate(year = 2018,
-           block = block.names[i])
+  sub[[i]] <- calc(Elevation_fill[[i]], function(x) {x - x[[1]]})
+  temp_sub <- sub[[i]] %>%
+    as.data.frame(xy = TRUE) %>%
+    gather(key = key, value = subsidence, 3:13) %>%
+    separate(key, into = c('block', 'year')) %>%
+    mutate(year = as.numeric(year))
   min.coords <- temp_sub %>%
     summarize(x.min = min(x),
               y.min = min(y))
   temp_sub <- temp_sub %>%
     mutate(long.norm = round(x - min.coords$x.min),
            lat.norm = round(y - min.coords$y.min))
-  sub_2018_df <- sub_2018_df %>%
+  sub_df <- sub_df %>%
     rbind.data.frame(temp_sub)
   temp_fences <- subset(Fences, block == unique(block)[i]) %>%
     mutate(long.norm = round(long - min.coords$x.min),
@@ -94,21 +120,76 @@ for (i in 1:length(Elevation_fill)) {
   Fences_norm <- Fences_norm %>%
     rbind.data.frame(temp_fences)
     avg_sub <- avg_sub %>%
-      rbind.data.frame(data.frame(year = 2018, 
-                                  block = block.names[i], 
-                                  avg.sub = cellStats(raster(sub_2018[[i]]), mean, na.rm = TRUE)))
+      rbind.data.frame(data.frame(key = names(sub[[i]]), 
+                                  avg.sub = cellStats(sub[[i]], mean, na.rm = TRUE)) %>%
+                         separate(key, into = c('block', 'year')) %>%
+                         mutate(year = as.numeric(year)))
+      
   rm(i, temp_fences, temp_sub)
 }
+#############################################################################################################
 
+### Calculate Latest Subsidence (DryPEHR) ###################################################################
+filenames <- list.files('C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/GPS/Kriged_Surfaces/Elevation_Variance/ALT_Sub_Ratio_Corrected/Elevation_Stacks', 
+                        full.names = TRUE,
+                        pattern = '[ABC]ElevStack_filled_unclipped.tif')
+Elevation_fill_drypehr <- list(brick(filenames[which(str_detect(filenames, 'AElevStack'))]), 
+                               brick(filenames[which(str_detect(filenames, 'BElevStack'))]), 
+                               brick(filenames[which(str_detect(filenames, 'CElevStack'))]))
+names(Elevation_fill_drypehr) <- list('Block A', 'Block B', 'Block C')
+names(Elevation_fill_drypehr[[1]]) <- paste('A', seq(2009, 2019), sep = '_')
+names(Elevation_fill_drypehr[[2]]) <- paste('B', seq(2009, 2019), sep = '_')
+names(Elevation_fill_drypehr[[3]]) <- paste('C', seq(2009, 2019), sep = '_')
 
-subsidence_map <- ggplot(sub_2018_df, aes(x=long.norm, y=lat.norm, fill=subsidence)) +
+Elevation_fill_drypehr <- map(Elevation_fill_drypehr, ~subset(.x, 3:11))
+Elevation_fill_drypehr_clipped <- map(Elevation_fill_drypehr, ~mask(.x, blocks11))
+
+sub_drypehr <- list()
+Fences_norm_drypehr <- data.frame()
+sub_df_drypehr <- data.frame()
+avg_sub_drypehr <- data.frame()
+for (i in 1:length(Elevation_fill_drypehr_clipped)) {
+  block.names <- c('A', 'B', 'C')
+  sub_drypehr[[i]] <- calc(Elevation_fill_drypehr_clipped[[i]], function(x) {x - x[[1]]})
+  temp_sub <- sub_drypehr[[i]] %>%
+    as.data.frame(xy = TRUE) %>%
+    gather(key = key, value = subsidence, 3:11) %>%
+    separate(key, into = c('block', 'year')) %>%
+    mutate(year = as.numeric(year))
+  min.coords <- temp_sub %>%
+    summarize(x.min = min(x),
+              y.min = min(y))
+  temp_sub <- temp_sub %>%
+    mutate(long.norm = round(x - min.coords$x.min),
+           lat.norm = round(y - min.coords$y.min))
+  sub_df_drypehr <- sub_df_drypehr %>%
+    rbind.data.frame(temp_sub)
+  temp_fences <- subset(Fences, block == unique(block)[i]) %>%
+    mutate(long.norm = round(long - min.coords$x.min),
+           lat.norm = round(lat - min.coords$y.min),
+           dummy = '')
+  Fences_norm <- Fences_norm %>%
+    rbind.data.frame(temp_fences)
+  avg_sub_drypehr <- avg_sub_drypehr %>%
+    rbind.data.frame(data.frame(key = names(sub[[i]]), 
+                                avg.sub = cellStats(sub[[i]], mean, na.rm = TRUE)) %>%
+                       separate(key, into = c('block', 'year')) %>%
+                       mutate(year = as.numeric(year)))
+  
+  rm(i, temp_fences, temp_sub)
+}
+#############################################################################################################
+
+### Graph subsidence (CiPEHR only) ##########################################################################
+subsidence_map_all_years <- ggplot(sub_df, aes(x=long.norm, y=lat.norm, fill=subsidence)) +
   geom_tile(aes(height = 2, width = 2)) + # have to set height and width due to bug. See note/link above.
   geom_path(data = Fences_norm, aes(x = long.norm, y = lat.norm, group = group, color = dummy), inherit.aes = FALSE) +
   # geom_text(data = avg_sub, aes(x = 11, y = 37.5, label = paste('Avg Sub = ', round(avg.sub, 2), 'm')), inherit.aes = FALSE) +
-  facet_grid(. ~ block) +
+  facet_grid(block ~ year) +
   theme_few() +
   scale_fill_viridis(expression(Delta*" Elevation (m)"),
-                     limits = c(-1.0, 0.2)) +
+                     limits = c(-1.0, 0.2),
+                     na.value = 'transparent') +
   scale_color_manual('Snow Fence',
                      values = 'black') +
   scale_x_continuous(name = 'Distance (m)') +
@@ -119,7 +200,30 @@ subsidence_map <- ggplot(sub_2018_df, aes(x=long.norm, y=lat.norm, fill=subsiden
         axis.title.y = element_text(size = 10),
         title = element_text(size = 16)) +
   coord_fixed() +
-  ggtitle('9 Years of Subsidence at CiPEHR')
+  ggtitle('10 Years of Subsidence at CiPEHR')
+
+subsidence_map_all_years
+
+subsidence_map <- ggplot(filter(sub_df, year == 2019), aes(x=long.norm, y=lat.norm, fill=subsidence)) +
+  geom_tile(aes(height = 2, width = 2)) + # have to set height and width due to bug. See note/link above.
+  geom_path(data = Fences_norm, aes(x = long.norm, y = lat.norm, group = group, color = dummy), inherit.aes = FALSE) +
+  # geom_text(data = avg_sub, aes(x = 11, y = 37.5, label = paste('Avg Sub = ', round(avg.sub, 2), 'm')), inherit.aes = FALSE) +
+  facet_grid(. ~ block) +
+  theme_few() +
+  scale_fill_viridis(expression(Delta*" Elevation (m)"),
+                     limits = c(-1.0, 0.2),
+                     na.value = 'transparent') +
+  scale_color_manual('Snow Fence',
+                     values = 'black') +
+  scale_x_continuous(name = 'Distance (m)') +
+  scale_y_continuous(name = 'Distance (m)') +
+  theme(aspect.ratio = 1,
+        plot.title = element_text(hjust = 0.5),
+        axis.title.x = element_text(size = 10),
+        axis.title.y = element_text(size = 10),
+        title = element_text(size = 16)) +
+  coord_fixed() +
+  ggtitle('10 Years of Subsidence at CiPEHR')
 
 subsidence_map
 
